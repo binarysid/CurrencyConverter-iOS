5ec475b6ebded2f58107fce1ad729c342ad17661
Linkon Sid <linkon@sheba.xyz>
2022-11-30 02:07:37 +0600

    unit test for currency conversion enabled and refactored

--- CurrencyConverterTests/CurrencyConverterTests.swift
+++ CurrencyConverterTests/CurrencyConverterTests.swift
@@ -9,34 +9,34 @@ import XCTest
 @testable import CurrencyConverter
 
 class CurrencyConverterTests: XCTestCase {
-//    var presenter:Presenter?
+    var worker:ConversionWorker?
     override func setUpWithError() throws {
-//        presenter = Presenter()
+        worker = ConversionWorker()
     }
     
     override func tearDownWithError() throws {
-//        presenter = nil
+        worker = nil
     }
-//    func testEUROToGBPonversion(){
-//        let expected = 0.87
-//        let units:Double = 1
-//        guard let val = presenter?.convert(givenCurrencyRateInUSD: 1.02956, expectedCurrencyRateInUSD: 0.900769, amount: units) else{return}
-//        
-//        XCTAssertTrue(val.checkEqual(to: expected, decimalPoint: 1))
-//    }
-//    func testGBPToPLNConversion(){
-//        let expected = 55.33
-//        let units:Double = 10
-//        guard let val = presenter?.convert(givenCurrencyRateInUSD: 0.901998, expectedCurrencyRateInUSD: 4.98899, amount: units) else{return}
-//        XCTAssertTrue(val.checkEqual(to: expected, decimalPoint: 1))
-//    }
-//    func testEURToCZKConversion(){
-//        let expected = 244.95
-//        let units:Double = 10
-//        guard let val = presenter?.convert(givenCurrencyRateInUSD: 1.026167, expectedCurrencyRateInUSD: 25.1363, amount: units) else{return}
-//        
-//        XCTAssertTrue(val.checkEqual(to: expected, decimalPoint: 1))
-//    }
+    func testEUROToGBPonversion(){
+        let expected = 0.87
+        let units:Double = 1
+        guard let val = worker?.convert(givenCurrencyRateInUSD: 1.02956, expectedCurrencyRateInUSD: 0.900769, amount: units) else{return}
+        
+        XCTAssertTrue(val.checkEqual(to: expected, decimalPoint: 1))
+    }
+    func testGBPToPLNConversion(){
+        let expected = 55.33
+        let units:Double = 10
+        guard let val = worker?.convert(givenCurrencyRateInUSD: 0.901998, expectedCurrencyRateInUSD: 4.98899, amount: units) else{return}
+        XCTAssertTrue(val.checkEqual(to: expected, decimalPoint: 1))
+    }
+    func testEURToCZKConversion(){
+        let expected = 244.95
+        let units:Double = 10
+        guard let val = worker?.convert(givenCurrencyRateInUSD: 1.026167, expectedCurrencyRateInUSD: 25.1363, amount: units) else{return}
+        
+        XCTAssertTrue(val.checkEqual(to: expected, decimalPoint: 1))
+    }
 //    func testPerformanceExample() throws {
 //        // This is an example of a performance test case.
 //        self.measure {
